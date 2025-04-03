936bd0270c1e7de9bd71d5a3dbd8b4eb2f21bb89
Sachin Mehrotra <sachinmehrotra06@gmail.com>
2025-04-03 19:19:37 +0200

    array modified

--- arrayIntro.cpp
+++ arrayIntro.cpp
@@ -3,7 +3,14 @@ int main() {
     int N,i=0;
     std::cin>>N;
     int *A = new int[N];
-    while(std::cin>>A[i++]);
+ //   while(std::cin>>A[i++]); // I don not know why this line is not working in code but working in hackerank
+  //  while(std::cout<<A[--N]<<' ' && N);
+    for(i=0; i < N; i++){
+        std::cin >> A[i];
+    }
+    // for(i=N-1; i >= 0; i--){
+    //     std::cout << A[i] << ' ';
+    // }
     while(std::cout<<A[--N]<<' ' && N);
     delete[] A;
     return 0;
